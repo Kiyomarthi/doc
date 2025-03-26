7d3d401ae467283179986e1b3050426e99ddc856
kiumarsi <a.kiyomarthi@gmail.com>
2025-03-26 12:27:13 +0330

    Fix root deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,16 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://kiyomarthi.github.io",  
+  url: "https://kiyomarthi.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/doc/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "kiyomarthi", // Usually your GitHub org/user name.
+  projectName: "kiyomarthi.github.io", // Usually your repo name.
+  trailingSlash: false, 
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
